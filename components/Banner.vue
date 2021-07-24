88fb85cb98075c34de60c288e65ce6ff9e150214
Rifat Osmani <rifat.osmani77@gmail.com>
2021-07-24 18:16:32 +0200

    commit

---
+++ components/Banner.vue
@@ -0,0 +1,117 @@
+<template>
+  <section>
+    <div
+      v-if="home === 0"
+      class="fullBackground"
+      :style="'background-image: url(' + this.url + ')'"
+    >
+      <div class="half">
+        <div
+          class="slide"
+          style="
+            opacity: 1;
+            visibility: visible;
+            position: relative;
+            right: 0px;
+          "
+        >
+          <div style="padding: 20% 0">
+            <h2
+              class="bannerText"
+              :style="
+                'color: ' +
+                this.colorText +
+                '; border-bottom: 1px solid ' +
+                this.colorText +
+                ';'
+              "
+            >
+              {{ title }}
+            </h2>
+            <h3
+              class="whiteTxt"
+              :style="'color: ' + this.colorText + ';text-align: center;'"
+            >
+              {{ descr }}
+            </h3>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div v-else class="fullBackgroundHome">
+      <div class="half">
+        <div
+          class="slide"
+          style="
+            opacity: 1;
+            visibility: visible;
+            position: relative;
+            right: 0px;
+          "
+        >
+          <div style="padding: 0% 0">
+            <h1 class="bannerText" style="font-size: 8em">
+              {{ title }}
+            </h1>
+          </div>
+        </div>
+      </div>
+      <div class="half">
+        <div
+          class="slide"
+          style="
+            opacity: 1;
+            visibility: visible;
+            position: relative;
+            right: 0px;
+          "
+        >
+          <div style="padding: 20% 0">
+            <h1 style="color: white">Global solutions.</h1>
+            <h1 style="color: white; margin-right: 5.5%">Innovations.</h1>
+            <h1 style="color: white; margin-right: 12%">Quality.</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+<script>
+export default {
+  props: ['title', 'descr', 'url', 'colorText', 'home'],
+}
+</script>
+<style scoped>
+.fullBackground {
+  width: 100%;
+  height: 400px;
+}
+.fullBackgroundHome {
+  background-color: rgba(73, 121, 255, 0.555);
+  width: 100%;
+  height: 400px;
+}
+.slide {
+  box-sizing: border-box;
+  text-align: center;
+}
+.half {
+  width: 46%;
+  float: left;
+  height: 100%;
+}
+
+h1 {
+  display: block;
+  font-size: 1.25em;
+  margin-block-start: 0.67em;
+  margin-block-end: 0.67em;
+  margin-inline-start: 0px;
+  margin-inline-end: 0px;
+  font-weight: bold;
+  text-align: center;
+}
+h3 {
+  margin-left: 60px;
+}
+</style>
---
+++ components/TheHeader.vue
@@ -0,0 +1,86 @@
+<template>
+  <header>
+    
+    <nav>
+      <ul>
+        <li id="logo">
+            <nuxt-link to="/">ICT COMPANY </nuxt-link>
+        </li>
+        <li>
+          <nuxt-link to="/people">Our Team</nuxt-link>
+        </li>
+        <li>
+          <nuxt-link to="/products">Our Products</nuxt-link>
+        </li>
+        <li>
+          <nuxt-link to="/areas">Our Areas</nuxt-link>
+        </li>
+        <li>
+          <nuxt-link to="/about">About Us</nuxt-link>
+        </li>
+        <li>
+          <nuxt-link to="/contacts">Contact Us</nuxt-link>
+        </li>
+      </ul>
+    </nav>
+    
+  </header>
+
+</template>
+
+<style scoped>
+header {
+  width: 100%;
+  height: 5rem;
+  background-color: #11005c;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+header a {
+  text-decoration: none;
+  color: #ffffff;
+  display: inline-block;
+  padding: 0.75rem;
+}
+
+a:active,
+a:hover,
+a.nuxt-link-active {
+  border: 1px solid #ffffff;
+}
+
+h1 {
+  margin: 0;
+}
+
+h1 a {
+  color: white;
+  margin: 0;
+}
+
+header nav {
+  width: 100%;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+}
+
+header ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  justify-content: space-between;
+}
+
+li {
+  margin: 0 auto;
+  flex-shrink: 5;
+}
+#logo{
+  background-color: rgb(22, 94, 230);
+}
+
+</style>
